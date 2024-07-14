04217d7592c01cacc5297286bc8e82819cc0758a
Profi_GMan <Profi_GMan@outlook.com>
2024-07-14 04:00:05 +0300

    Add .config/yazi/init.lua Add .config/yazi/package.toml Add .config/yazi/plugins/archive.yazi/LICENSE Add .config/yazi/plugins/archive.yazi/README.md Add .config/yazi/plugins/archive.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/archive.yazi/init.lua Add .config/yazi/plugins/chmod.yazi/LICENSE Add .config/yazi/plugins/chmod.yazi/README.md Add .config/yazi/plugins/chmod.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/chmod.yazi/init.lua Add .config/yazi/plugins/fast-enter.yazi/LICENSE Add .config/yazi/plugins/fast-enter.yazi/README.md Add .config/yazi/plugins/fast-enter.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/fast-enter.yazi/init.lua Add .config/yazi/plugins/full-border.yazi/LICENSE Add .config/yazi/plugins/full-border.yazi/README.md Add .config/yazi/plugins/full-border.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/full-border.yazi/init.lua Add .config/yazi/plugins/jump-to-char.yazi/LICENSE Add .config/yazi/plugins/jump-to-char.yazi/README.md Add .config/yazi/plugins/jump-to-char.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/jump-to-char.yazi/init.lua Add .config/yazi/plugins/lsar.yazi/LICENSE Add .config/yazi/plugins/lsar.yazi/README.md Add .config/yazi/plugins/lsar.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/lsar.yazi/init.lua Add .config/yazi/plugins/max-preview.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/relative-motions.yazi/LICENSE Add .config/yazi/plugins/relative-motions.yazi/README.md Add .config/yazi/plugins/relative-motions.yazi/DO_NOT_MODIFY_ANYTHING_IN_THIS_DIRECTORY Add .config/yazi/plugins/relative-motions.yazi/init.lua

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,16 @@
+require("full-border"):setup()
+
+-- require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+
+function Status:name()
+	local h = cx.active.current.hovered
+	if not h then
+		return ui.Span("")
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Span(" " .. h.name .. linked)
+end
