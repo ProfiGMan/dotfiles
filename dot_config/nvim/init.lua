f448019f772be9833cd3ac24207a02c588d181a2
Profi_GMan <profi_gman@outlook.com>
2024-07-28 13:17:47 +0300

    upd

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,11 @@
+--local opt = vim.opt
+
+--opt.clipboard = 'unnamedplus'
+-- nnoremap("<C-d>", "<C-d>zz")
+-- nnoremap("<C-u>", "<C-u>zz")
+-- nnoremap("n", "nzz")
+-- nnoremap("N", "Nzz")
+-- xnoremap("<leader>p", "\"_dP")
+
+require("config.lazy")
+
