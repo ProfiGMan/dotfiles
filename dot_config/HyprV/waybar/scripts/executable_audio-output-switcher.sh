7e4c66d1a7371f7c1b0681228191f8d0a08c2759
Profi_GMan <Profi_GMan@outlook.com>
2024-08-06 22:31:39 +0300

    Update .config/HyprV/waybar/scripts/audio-output-switcher.sh Update .config/hypr/hypridle.conf Update .config/hypr/hyprland.conf Update .config/keyd/default.conf Update .config/nvim/lazy-lock.json Update .config/tridactyl/tridactylrc Update .config/yazi/keymap.toml

--- dot_config/HyprV/waybar/scripts/executable_audio-output-switcher.sh
+++ dot_config/HyprV/waybar/scripts/executable_audio-output-switcher.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 active_port=$(pactl list sinks | grep "Active Port: analog" | sed 's/^.*analog-output-//')
+sink=$(pactl list sinks | grep -B 1 "State: RUNNING" | sed -n -e 's/Sink #//p')
 
 if [ $active_port == 'lineout' ]; then
-	pactl set-sink-port 1 analog-output-headphones
+	pactl set-sink-port $sink analog-output-headphones
 else
-	pactl set-sink-port 1 analog-output-lineout
+	pactl set-sink-port $sink analog-output-lineout
 fi
